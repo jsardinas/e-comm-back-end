339532ebfcb6d3089c4d74ab1ed66cd4cc76824b
Javier <javier.sardinas@gmail.com>
2022-06-15 09:58:31 -0500

    Implement delete route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -97,8 +97,14 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  var deleted = await Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+  res.status(200).json(deleted);
 });
 
 module.exports = router;
