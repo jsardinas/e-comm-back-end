98b1612fe28e3be32809961d92e4b7ac8387b921
Javier <javier.sardinas@gmail.com>
2022-06-18 09:22:32 -0500

    Add error responses to category queries

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,50 +6,89 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  var categories = await Category.findAll({
-    include: [{model: Product}]
-  });
-  categories = categories.map(x => x.dataValues);
-  res.status(200).json(categories);
+  try{
+    var categories = await Category.findAll({
+      include: [{model: Product}]
+    });
+    categories = categories.map(x => x.dataValues);
+    res.status(200).json(categories);
+  }
+  catch(e){
+    res.status(500).json(e);
+  }
 });
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  var category = await Category.findByPk(req.params.id, {
-    include: [{ model: Product }],
-  });
-  res.status(200).send(category.dataValues);
+  try{
+    var category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (category === null)
+      res.status(404).json('Category id not found');
+    else
+      res.status(200).json(category.dataValues);
+  }
+  catch(e){
+    console.log(e);
+    res.status(500).json(e);
+  }
 });
 
 router.post('/', async (req, res) => {
   // create a new category
-  if (req.body && req.body.category_name){
-    var category = await Category.create(req.body);
-    res.status(200).json(category);
+  try{
+    if (req.body && req.body.category_name){
+      var category = await Category.create(req.body);
+      res.status(201).json(category);
+    }
+    else{
+      res.status(400).json('Missing category name');
+    }
   }
+  catch(e){
+    res.status(500).json(e);
+  }
+  
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  if (req.body && req.body.category_name){
-    var category = await Category.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    });
-    res.status(200).json(category);
+  try{
+    if (req.body && req.body.category_name){
+      var category = await Category.update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      });
+      res.status(200).json(category);
+    }
+    else
+      res.status(400).json('Missing category name');
   }
+  catch(e){
+    res.status(500).json(e);
+  }
+  
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-  var deleted = await Category.destroy({
-    where: {
-      id: req.params.id
-    }
-  });
-  res.status(200).json(deleted);
+  try{
+    var deleted = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(deleted === 1)
+      res.status(200).json(`Category with id ${req.params.id} has been deleted`);
+    else
+      res.status(404).json('Category id not found');
+  }
+  catch(e){
+    res.status(500).json(e);
+  }
 });
 
 module.exports = router;
